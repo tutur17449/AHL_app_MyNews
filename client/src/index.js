1ef0ee2dc676c4fc5103164f8d990c0cfc7e8370
Arthur HAMEL <>
2020-04-08 20:57:25 +0200

    fix icon

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,13 @@ import mainPages from './components/mainPages'
 import { initLoading } from './tools/displayLoading';
 import './assets/css/style.css'
 import loader from './assets/img/loading.gif'
+import icon from './assets/img/live-news.svg'
 
 
 const root = document.querySelector('#root')
 const loading = document.querySelector('#loading')
+const linkIcon = document.querySelector('link[rel="icon"]')
+linkIcon.setAttribute('href', icon)
 
 const router = () => {
     if(window.location.pathname === '/'){
